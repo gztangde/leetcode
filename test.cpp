e6581a652c19b93b63422e7daa55d3ccf9bcb323
guozetang <guozetang@gmail.com>
2019-03-08 15:01:09 -0500

    [AUTO]guoze-chromebook commit at 2019-03-08 15:01:08

--- test.cpp
+++ test.cpp
@@ -1,9 +1,41 @@
+// Follow up
 class Solution {
  public:
-  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int p = m - 1, q = n - 1, merge = m + n - 1;
-    while (p >= 0 && q >= 0) nums1[merge--] = nums1[p] >= nums2[q] ? nums1[p--] : nums2[q--];
-    // If the p == 0
-    while (q >= 0) nums1[merge--] = nums2[q--];
+  struct MyTreeNode {
+    int val;
+    int count;
+    MyTreeNode* left;
+    MyTreeNode* right;
+    MyTreeNode(int x) : val(x), count(1), left(NULL), right(NULL) {}
+  };
+
+  MyTreeNode* build(TreeNode* root) {
+    if (!root) return NULL;
+    MyTreeNode* node = new MyTreeNode(root->val);
+    node->left = build(root->left);
+    node->right = build(root->right);
+    if (node->left) node->count += node->left->count;
+    if (node->right) node->count += node->right->count;
+    return node;
+  }
+
+  int kthSmallest(TreeNode* root, int k) {
+    MyTreeNode* node = build(root);
+    return helper(node, k);
+  }
+
+  int helper(MyTreeNode* node, int k) {
+    if (node->left) {
+      int cnt = node->left->count;
+      if (k <= cnt) {
+        return helper(node->left, k);
+      } else if (k > cnt + 1) {
+        return helper(node->right, k - 1 - cnt);
+      }
+      return node->val;
+    } else {
+      if (k == 1) return node->val;
+      return helper(node->right, k - 1);
+    }
   }
 };
