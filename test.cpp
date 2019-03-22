66d8ec26fee81946ef1e4729899cefec3a165ff1
guozetang <guozetang@gmail.com>
2019-03-22 16:38:16 -0400

    [AUTO]guoze-chromebook commit at 2019-03-22 16:38:15

--- test.cpp
+++ test.cpp
@@ -1,45 +1,16 @@
 class Solution {
  public:
-  string longestPalindrome(string s) {
-    string Q = "$#";
-    for (int i = 0; i < s.size(); ++i) {
-      Q += s[i];
-      Q += '#';
+  int rob(vector<int>& nums) {
+    if (nums.size() <= 1) return nums.empty() ? 0 : nums[0];
+    return max(rob(nums, 0, nums.size() - 1), rob(nums, 1, nums.size()));
+  }
+  int rob(vector<int>& nums, int left, int right) {
+    int rob = 0, notRob = 0;
+    for (int i = left; i < right; ++i) {
+      int preRob = rob, preNotRob = notRob;
+      rob = preNotRob + nums[i];
+      notRob = max(preRob, preNotRob);
     }
-    int c = 0, r = 0;  // current center, right limit
-    int P[Q.size()] = {0};
-    for (int i = 1; i < Q.size() - 1; i++) {
-      // find the corresponding letter in the palidrome subString
-      int iMirror = c - (i - c);
-
-      if (r > i) {
-        P[i] = min(r - i, P[iMirror]);
-      }
-
-      // expanding around center i
-      while (Q[i + 1 + P[i]] == Q[i - 1 - P[i]]) {
-        P[i]++;
-      }
-
-      // Update c,r in case if the palindrome centered at i expands past r,
-      if (i + P[i] > r) {
-        c = i;  // next center = i
-        r = i + P[i];
-      }
-    }
-
-    // Find the longest palindrome length in p.
-
-    int maxPalindrome = 0;
-    int centerIndex = 0;
-
-    for (int i = 1; i < Q.size() - 1; i++) {
-      if (P[i] > maxPalindrome) {
-        maxPalindrome = P[i];
-        centerIndex = i;
-      }
-    }
-
-    return s.substr((centerIndex - 1 - maxPalindrome) / 2, maxPalindrome);
+    return max(rob, notRob);
   }
 };
