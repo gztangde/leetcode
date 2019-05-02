0683f695f7c762c9173d9b088fa5aef5eb4c0313
guozetang <guozetang@gmail.com>
2019-05-02 15:08:15 -0400

    Update the code

--- GitbookCommon.mk
+++ GitbookCommon.mk
@@ -336,7 +336,7 @@ push:
   git remote add origin https://${REF}  && \
   git add .  && \
   git commit -m "Updated By Travis-CI With Build $TRAVIS_BUILD_NUMBER For Github Pages"  && \
-  git push --force --quiet "https://${TOKEN}@${REF}" master:gh-pages  && \
+  git push --force --quiet "https://${TOKEN}@${REF}" master:gh-pages
 
 ################################################################################
 # Help
